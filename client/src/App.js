c3a95a30c6f7fcc36e2d51fa65cbff5b3840b43e
chriscrichter <chris_richter@outlook.com>
2020-03-11 14:04:51 -0400

    Created base files.

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+// The app will not render correctly until you setup a Route component.
+// Refer to the Basic Example documentation if you need to.
+// (https://reacttraining.com/react-router/web/example/basic)
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
